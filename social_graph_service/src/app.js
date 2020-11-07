305a7898fd4eb4c466c30260bdf7822d3ddc986a
birgitterl <schneiderbirgit@gmx.at>
2020-11-07 15:34:17 +0100

    implemented realtime updates on dev environment

--- social_graph_service/src/app.js
+++ social_graph_service/src/app.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-
-app.get('/', function (req, res) {
-    res.send('Hi there social-graph-service works');
+app.get("/", function (req, res) {
+  res.send("Nicy Nice - Hi there social-graph-service works");
 });
 
 module.exports = app;
