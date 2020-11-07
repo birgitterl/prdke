dbf10a0d130e4bfaacaf891e37714a1077b08c77
birgitterl <schneiderbirgit@gmx.at>
2020-11-07 15:36:32 +0100

    realtime updates on all service in dev environment

--- auth_service/server.js
+++ auth_service/server.js
@@ -1,11 +1,10 @@
-const app = require('./src/app')
-const connectDB = require('./src/config/db');
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
 //Connect to DB
 connectDB();
 
 // Connect to port
 app.listen(8080, () => {
-    console.log('Server started on port 8080...');
+  console.log("Server started on port 8080...");
 });
-
