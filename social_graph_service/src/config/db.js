c322ffb53e77f7148967122d737d7c57616cec5a
birgitterl <schneiderbirgit@gmx.at>
2020-11-07 06:04:14 +0100

    neo4j settings updated

--- social_graph_service/src/config/db.js
+++ social_graph_service/src/config/db.js
@@ -1,4 +1,5 @@
 const neo4j = require('neo4j-driver');
 
-const driver = neo4j.driver('bolt://neo4j:7687', neo4j.auth.basic('neo4j', '12345')); //TODO password
+// credentials: neo4j/admin
+const driver = neo4j.driver('bolt://graphDB:7687'); //TODO password
 const session = driver.session;
