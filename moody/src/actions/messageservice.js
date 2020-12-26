efcdcd30b27730578f86776e2197b01bc7d69451
Gerstlacher <j.gerstlacher@gmx.de>
2020-12-26 21:03:49 +0100

    Modified actions for fetching messages (timelines)

--- moody/src/actions/messageservice.js
+++ moody/src/actions/messageservice.js
@@ -1,8 +1,13 @@
-// User timeline: Fetch my messages
+// User timeline = Fetch my messages, Home timeline = Fetch messages of a profile I follow
 
 import socialGraphService from '../utils/socialGraphService';
 
-import { GET_MY_MESSAGES, GET_OTHER_MESSAGES, MESSAGE_ERROR } from './types';
+import {
+  GET_MY_MESSAGES,
+  GET_OTHER_MESSAGES,
+  GET_MESSAGES_FOLLOWED_PROFILE,
+  MESSAGE_ERROR
+} from './types';
 
 // Get the users current messages
 export const getMyMessages = () => async (dispatch) => {
@@ -28,6 +33,25 @@ export const getOtherMessages = () => async (dispatch) => {
 
     dispatch({
       type: GET_OTHER_MESSAGES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: MESSAGE_ERROR,
+      payload: { msg: err.message, status: err.status }
+    });
+  }
+};
+
+// Get the last 25 messages of a specific profile I follow
+export const getMessagesFromProfileIFollow = (username) => async (dispatch) => {
+  try {
+    const res = await socialGraphService.get(
+      '/messages/followedProfile?$username'
+    );
+
+    dispatch({
+      type: GET_MESSAGES_FOLLOWED_PROFILE,
       payload: res.data
     });
   } catch (err) {
