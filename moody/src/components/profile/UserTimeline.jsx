1ec78657d494806ce715adcd359af10e56740b69
Gerstlacher <j.gerstlacher@gmx.de>
2020-12-28 19:57:49 +0100

    First version of user timeline implementation.

--- moody/src/components/profile/UserTimeline.jsx
+++ moody/src/components/profile/UserTimeline.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Card, Container, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { getMyMessages } from '../../actions/messageservice';
 
-const UserTimeline = () => {
+const UserTimeline = ({
+  auth: { user },
+  myposts: { messages, loading },
+  getMyMessages
+}) => {
+  useEffect(() => {
+    getMyMessages();
+  }, [getMyMessages]);
   return (
-    <div>
-      <p>This is the UserTimeline component</p>
-    </div>
+    <Container>
+      <h3>My messages/post</h3>
+
+      <Card>
+        <Card.Header>Messages of {user.username}</Card.Header>
+        <Container fluid></Container>
+
+        <ListGroup>
+          {messages.map((post, index) => (
+            <ListGroupItem key={index}>
+              {`${post.text} posted on ${post.timestamp}`}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </Card>
+    </Container>
   );
 };
 
-export default UserTimeline;
+UserTimeline.propTypes = {
+  getMyMessages: PropTypes.func.isRequired,
+  myposts: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  myposts: state.myposts,
+  auth: state.auth
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getMyMessages: () => dispatch(getMyMessages())
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(UserTimeline);
---
+++ moody/src/reducers/usertimeline.js
@@ -0,0 +1,36 @@
+import { GET_MY_MESSAGES, MESSAGE_ERROR } from '../actions/types';
+
+const initialState = {
+  //property = props
+  // loading flag - data is fetched or not
+  loading: true,
+  // data: array
+  messages: [],
+  // error:''
+  error: {}
+};
+
+const message = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_MY_MESSAGES:
+      return {
+        // copy of the state object
+        ...state,
+        loading: false,
+        messages: payload
+      };
+    case MESSAGE_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+        messages: null
+      };
+    default:
+      return state;
+  }
+};
+
+export default message;
