5827c1af00afcf2bb1f27073b188909905d06689
Skrolux <46719449+Skrolux@users.noreply.github.com>
2021-01-06 18:17:17 +0100

    Update userprofile.js

--- moody/src/reducers/userprofile.js
+++ moody/src/reducers/userprofile.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 /* import { GET_PROFILEOFINTEREST, PROFILE_ERROR } from '../actions/types';
-=======
-import { GET_PROFILEOFINTEREST, PROFILE_ERROR } from '../actions/types';
->>>>>>> cae8b56ddf46ebf2037e82f0b347f3c7b7154745
 
 const initialState = {
   profile: null,
@@ -33,7 +29,4 @@ const profileOfInterest = (state = initialState, action) => {
 };
 
 export default profileOfInterest;
-<<<<<<< HEAD
- */
-=======
->>>>>>> cae8b56ddf46ebf2037e82f0b347f3c7b7154745
+*/
