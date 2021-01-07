d93d753997feca88c16a3cdeb4ae3e92955d1d9d
Gerstlacher <j.gerstlacher@gmx.de>
2021-01-07 17:07:29 +0100

    Bug fixes message sorting

--- moody/src/actions/messageservice.js
+++ moody/src/actions/messageservice.js
@@ -3,76 +3,95 @@
 import socialGraphService from '../utils/socialGraphService';
 
 import {
-    GET_MY_MESSAGES,
-    GET_OTHER_MESSAGES,
-    GET_MESSAGES_FOLLOWED_PROFILE,
-    MESSAGE_ERROR
+  GET_MY_MESSAGES,
+  GET_OTHER_MESSAGES,
+  GET_MESSAGES_FOLLOWED_PROFILE,
+  MESSAGE_ERROR
 } from './types';
 
 // Get the users current messages
-export const getMyMessages = () => async(dispatch) => {
-    try {
-        const res = await socialGraphService.get('/messages/my');
-
-        dispatch({
-            type: GET_MY_MESSAGES,
-            payload: res.data
-        });
-    } catch (err) {
-        dispatch({
-            type: MESSAGE_ERROR,
-            payload: { msg: err.message, status: err.status }
-        });
-    }
+export const getMyMessages = () => async (dispatch) => {
+  try {
+    const res = await socialGraphService.get('/messages/my');
+    var resultSet = sortMessages(res.data);
+    dispatch({
+      type: GET_MY_MESSAGES,
+      payload: resultSet
+    });
+  } catch (err) {
+    dispatch({
+      type: MESSAGE_ERROR,
+      payload: { msg: err.message, status: err.status }
+    });
+  }
 };
 
-// Get the last 25 messages of profiles I follow
-export const getOtherMessages = () => async(dispatch) => {
-    try {
-        const res = await socialGraphService.get('/messages/other');
-
-        dispatch({
-            type: GET_OTHER_MESSAGES,
-            payload: res.data
-        });
-    } catch (err) {
-        dispatch({
-            type: MESSAGE_ERROR,
-            payload: { msg: err.message, status: err.status }
-        });
-    }
+// Get the last 100 messages of profiles I follow
+export const getOtherMessages = () => async (dispatch) => {
+  try {
+    const res = await socialGraphService.get('/messages/other');
+    var resultSet = sortMessages(res.data);
+    dispatch({
+      type: GET_OTHER_MESSAGES,
+      payload: resultSet
+    });
+  } catch (err) {
+    dispatch({
+      type: MESSAGE_ERROR,
+      payload: { msg: err.message, status: err.status }
+    });
+  }
 };
+
+// Get the last 30 messages of a specific profile I follow
+export const getMessagesFromProfileIFollow = (username) => async (dispatch) => {
+  try {
+    const res = await socialGraphService.get(
+      '/messages/followedProfile?$username'
+    );
+    var resultSet = sortMessages(res.data);
+    console.log(resultSet);
+    dispatch({
+      type: GET_MESSAGES_FOLLOWED_PROFILE,
+      payload: resultSet
+    });
+  } catch (err) {
+    dispatch({
+      type: MESSAGE_ERROR,
+      payload: { msg: err.message, status: err.status }
+    });
+  }
+};
+
+function sortMessages(messageArray) {
+  // copy res.data to resultArray for sorting
+  var resultArray = messageArray;
+  for (var i = 0; i < messageArray.length; i++) {
+    resultArray[i] = messageArray[i];
+    resultArray[i].timestamp = new Date(resultArray[i].timestamp);
+  }
+
+  // sort results by timestamp in ASC order
+  var sortedArray = resultArray.sort(function (a, b) {
+    return b.timestamp - a.timestamp;
+  });
+  console.log(resultArray);
+  console.log(sortedArray);
+  return sortedArray;
+}
 
 // Post new Message
 export async function postMessage(msg) {
-    try {
-        const res = await socialGraphService.post('messages', {
-                author: msg.author,
-                text: msg.text
-            })
-            .then(res => {
-                console.log(res);
-            });
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-// Get the last 25 messages of a specific profile I follow
-export const getMessagesFromProfileIFollow = (username) => async(dispatch) => {
-    try {
-        const res = await socialGraphService.get(
-            '/messages/followedProfile?$username'
-        );
-
-        dispatch({
-            type: GET_MESSAGES_FOLLOWED_PROFILE,
-            payload: res.data
-        });
-    } catch (err) {
-        dispatch({
-            type: MESSAGE_ERROR,
-            payload: { msg: err.message, status: err.status }
-        });
-    }
-};
+  try {
+    const res = await socialGraphService
+      .post('messages', {
+        author: msg.author,
+        text: msg.text
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+}
